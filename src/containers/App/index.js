c8736b5c87143e834a2e53fe9ab854732f56bbc7
karatehcops <contact@lazers.tv>
2018-03-27 16:06:32 -0400

    Update lint rules.

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -27,7 +27,7 @@ class App extends Component {
               <Switch>
                 <Route
                   exact
-                  path="/"
+                  path='/'
                   render={() =>
                     <Paragraph
                       textAlign='center'
